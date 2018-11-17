c50f6af3b9544e5acec66727592ef839a97c902f
UnknownWellFail <eugene.barannik37@gmail.com>
2018-11-17 21:41:48 +0300

    Added function for get 5 most popular words in comments

--- routes/api/comments.js
+++ routes/api/comments.js
@@ -10,34 +10,62 @@ router.post('/', function (req, res) {
             return console.dir(error);
         }
         let test = JSON.parse(body);
-
-        res.send({popularAuthor: getMaxComments(test), excetionTime: new Date().getTime() - startTime});
+        res.send({
+            popularAuthor: getMaxComments(test),
+            popularWords: getTopWords(test),
+            excetionTime: new Date().getTime() - startTime
+        });
     });
 });
 
 
-getMaxComments = (arr) => {
+function getMaxComments(arr) {
     let tmp = [];
-    let index = -1;
-    arr.forEach((msg) => {
+    let index;
+    for (msg of arr) {
         index = tmp.findIndex(mssg => mssg.email === msg.email);
         if (index !== -1) {
             tmp[index].count += 1;
         } else {
             tmp[tmp.length] = {email: msg.email, count: 1};
         }
-    });
-
+    }
     let max = {email: tmp[0].email, comments: tmp[0].count};
-
-    tmp.forEach((item) => {
+    for (item of tmp) {
         if (item.count > max.count) {
             max.popularAuthor.comments = item.count;
             max.popularAuthor.email = item.email;
         }
-    });
+    }
     return max;
-};
+}
+
+function getTopWords(arr) {
+    let words = [];
+    for (msg of arr) {
+        words = words.concat(msg.body.split(/\W/).filter(String));
+    }
+    let res = [];
+    let index;
+    for (word of words) {
+        index = res.findIndex(w => w.word === word);
+        if (index !== -1) {
+            res[index].count += 1;
+        } else {
+            res[res.length] = {word: word, count: 1};
+        }
+    }
+    res.sort((a, b) => {
+        if (a.count < b.count) return 1;
+        if (a.count > b.count) return -1;
+    });
+    res = res.slice(0, 5);
+    let result = {};
+    for (item of res) {
+        result[item.word] = item.count;
+    }
+    return result;
+}
 
 
 module.exports = router;
