d3fab6ec7aa6abd0316e4a6b09b38deabe8aa5fe
UnknownWellFail <eugene.barannik37@gmail.com>
2018-11-19 16:37:14 +0300

    ES 6 Error catching

--- app.js
+++ app.js
@@ -1,39 +1,27 @@
-let createError = require('http-errors');
-let express = require('express');
-let path = require('path');
-let cookieParser = require('cookie-parser');
-let logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-let indexRouter = require('./routes/api/comments');
+const indexRouter = require('./routes/api/comments');
 
-let app = express();
+const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/comments', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use(function (err, req, res) {
+  res.status(err.status || 500).send(err.message);
 });
 
 module.exports = app;
--- routes/api/comments.js
+++ routes/api/comments.js
@@ -1,21 +1,23 @@
-let express = require('express');
-let Request = require("request");
-let router = express.Router();
+const express = require('express');
+const Request = require('request');
+
+const router = express.Router();
 
 /* POST request*/
 router.post('/', function (req, res) {
-    let startTime = new Date().getTime();
-    Request.get("https://jsonplaceholder.typicode.com/comments", (error, response, body) => {
-        if (error) {
-            return console.dir(error);
-        }
-        let data = JSON.parse(body);
-        res.send({
-            popularAuthor: getMaxComments(data),
-            popularWords: getTopWords(data, 5),
-            executionTime: new Date().getTime() - startTime
-        });
+  let startTime = new Date().getTime();
+  Request.get('https://jsonplaceholder.typicode.com/comments', (error, response, body) => {
+    if (error) {
+      res.send(error.message);
+      return console.dir(error.message);
+    }
+    const data = JSON.parse(body);
+    res.send({
+      popularAuthor: getMaxComments(data),
+      popularWords: getTopWords(data, 5),
+      executionTime: new Date().getTime() - startTime
     });
+  });
 });
 
 /**
@@ -27,38 +29,38 @@ router.post('/', function (req, res) {
  * @param indexKey - used or not key in comparing data
  * @returns {Array}
  */
-function generateObjArray(key, value, array, indexKey) {
-    let result = [];
-    let index;
-    for (msg of array) {
-        if (indexKey)
-            index = result.findIndex(mssg => mssg[key] === msg[key]);
-        else
-            index = result.findIndex(mssg => mssg[key] === msg);
-        if (index !== -1) {
-            result[index][value] += 1;
-        } else {
-            if (indexKey)
-                result[result.length] = {[key]: msg[key], [value]: 1};
-            else
-                result[result.length] = {[key]: msg, [value]: 1};
-        }
+generateObjArray = (key, value, array, indexKey) => {
+  const result = [];
+  let index;
+  for (msg of array) {
+    if (indexKey)
+      index = result.findIndex(mssg => mssg[key] === msg[key]);
+    else
+      index = result.findIndex(mssg => mssg[key] === msg);
+    if (index !== -1) {
+      result[index][value] += 1;
+    } else {
+      if (indexKey)
+        result[result.length] = {[key]: msg[key], [value]: 1};
+      else
+        result[result.length] = {[key]: msg, [value]: 1};
     }
-    return result;
-}
+  }
+  return result;
+};
 
 /**
  * Converts comments array to array with all words from body of comment
  * @param array - comments data array
  * @returns Words {Array}
  */
-function getWordsFromDataBody(array) {
-    let words = [];
-    for (msg of array) {
-        words = words.concat(msg.body.split(/\W/).filter(String));
-    }
-    return words;
-}
+getWordsFromDataBody = (array) => {
+  let words = [];
+  for (msg of array) {
+    words = words.concat(msg.body.split(/\W/).filter(String));
+  }
+  return words;
+};
 
 
 /**
@@ -66,17 +68,17 @@ function getWordsFromDataBody(array) {
  * @param arr - comments data array
  * @returns object as {email:'',comments:''}
  */
-function getMaxComments(arr) {
-    let array = generateObjArray('email', 'count', arr, true);
-    let max = {email: array[0].email, comments: array[0].count};
-    for (item of array) {
-        if (item.count > max.count) {
-            max.popularAuthor.comments = item.count;
-            max.popularAuthor.email = item.email;
-        }
+getMaxComments = (arr) => {
+  const array = generateObjArray('email', 'count', arr, true);
+  const max = {email: array[0].email, comments: array[0].count};
+  for (item of array) {
+    if (item.count > max.count) {
+      max.popularAuthor.comments = item.count;
+      max.popularAuthor.email = item.email;
     }
-    return max;
-}
+  }
+  return max;
+};
 
 /**
  * Getting top @limit words from comments data array
@@ -84,22 +86,22 @@ function getMaxComments(arr) {
  * @param limit - required number of values
  * @returns array as {word: number of words}
  */
-function getTopWords(arr, limit) {
-    let words = getWordsFromDataBody(arr);
-    let res = generateObjArray('word', 'count', words, false);
+getTopWords = (arr, limit) => {
+  const words = getWordsFromDataBody(arr);
+  let res = generateObjArray('word', 'count', words, false);
 
-    //Sorting and shaping output
-    res.sort((a, b) => {
-        if (a.count < b.count) return 1;
-        if (a.count > b.count) return -1;
-    });
-    res = res.slice(0, limit);
-    let result = {};
-    for (item of res) {
-        result[item.word] = item.count;
-    }
-    return result;
-}
+  //Sorting and shaping output
+  res.sort((a, b) => {
+    if (a.count < b.count) return 1;
+    if (a.count > b.count) return -1;
+  });
+  res = res.slice(0, limit);
 
+  const result = {};
+  for (item of res) {
+    result[item.word] = item.count;
+  }
+  return result;
+};
 
 module.exports = router;
